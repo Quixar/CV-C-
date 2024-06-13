af574fbfcc267cbbeb326d294724fee3625fb75c
Quixar <145463262+Quixar@users.noreply.github.com>
2024-06-13 12:44:07 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\tCV\n");
+            Console.WriteLine("Martynov Yevhenii\n");
+            Console.WriteLine("Contacts:");
+            Console.WriteLine("Phone: +40792883327");
+            Console.WriteLine("email: evgenmart01@gmail.com\n");
+            Console.WriteLine("Languages:");
+            Console.WriteLine("English (B2)");
+            Console.WriteLine("Romanian (B2)\n");
+            Console.WriteLine("Skills:");
+            Console.WriteLine("Junior C++");
+            Console.WriteLine("Worked with UML");
+            Console.WriteLine("Knowledge of patterns\n");
+            Console.WriteLine("Education:");
+            Console.WriteLine("Bucharest, University of Bucharest");
+            Console.WriteLine("Odesa, IT school STEP");
+        }
+    }
+}
